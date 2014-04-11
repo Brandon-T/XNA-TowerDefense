dc46a0c57c125dd4f14f5166031049e0c9c51ab3
Brandon-T <Brandon.T-@Live.com>
2014-04-10 20:16:46 -0400

    Initial commit

---
+++ Assignment-2D/Assignment-2D/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment-2D")]
+[assembly: AssemblyProduct("Assignment-2D")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("b5bf5f2f-3f2c-405f-8cc0-4f2f49417c22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -2,3 +2,11 @@ XNA-TowerDefense
 ================
 
 A 2-D TowerDefense game using XNA
+
+
+Screenshots:
+------------
+
+![Screenshot1](Screenshots/1.png) ![Screenshot2](Screenshots/2.png)
+
+![Screenshot1](Screenshots/3.png)
